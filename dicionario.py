5797d7f29a0da09863c7d2f106ea31e224fc0803
Taliane Sousa <taliane62221636@alunos.sc.senac.br>
2026-03-16 21:50:42 -0300

    dicionario

---
+++ dicionario.py
@@ -0,0 +1,13 @@
+aluna = {'id': 1, 'nome': 'Taliane', 'nota': 10.0}
+pessoa = {'nome': 'Maria', 'idade': 24}
+
+print('Nome da pessoa:', pessoa['nome'])
+print('Nome da aluna:', aluna['nome'])
+print('Nota da aluna:', aluna['nota'])
+
+pessoa['cidade'] = 'Florianopolis'
+pessoa['idade'] = 26
+print('Pessoa atualizada:', pessoa)
+
+
+
